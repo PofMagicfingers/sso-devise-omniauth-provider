045d22b1225cbef7917e8abd0d43e94da2af0bbc
Pof Magicfingers <pof@pof.pm>
2013-08-17 02:34:56 +0200

    Adding redirect on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < Devise::SessionsController
-
+  private
+    def after_sign_out_path_for(resource_or_scope)
+      params[:redirect_uri].blank? ? super : params[:redirect_uri]
+    end
 end
 
